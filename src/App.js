d751307bc84973386bf2678c3991ff3b867ff1d2
남의영 <ny2485@naver.com>
2024-01-07 22:08:54 +0900

    props, 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+// import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
+  // let nameByLet = 'react test1';
+  // const nameByConst = 'react test2';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial/>
+      <Hello color="pink"/>
+      <h2>123213</h2>
+    </Wrapper>
   );
 }
 
